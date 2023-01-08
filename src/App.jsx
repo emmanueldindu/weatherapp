88bc2f11aa2cf4ad7addc9afa1e25423685f74a8
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-01-08 09:26:43 +0100

    👌 IMPROVE: weather app

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,40 @@ const api = {
 }
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday"
+    ];
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day}  ${date} ${month} ${year}`
+    
+}
+
   return (
     <div className="app">
       <main>
@@ -16,12 +50,13 @@ function App() {
             placeholder='search'
             className="search-bar" />
         </div>
-        <div className="">
+        <div className=""> 
           <div className="location-box">
             <div className="location">
               New York City, US
             </div>
             <div className="date">
+              {dateBuilder(new Date())}
               
             </div>
           </div>
